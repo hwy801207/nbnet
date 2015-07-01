2378a263a65ca47f6589eadd0a312e280d56c1dc
hwy801207 <19130258@qq.com>
2015-07-01 14:47:05 +0800

    add utf-8 encode support

--- NbNetBase/NetBase.py
+++ NbNetBase/NetBase.py
@@ -1,4 +1,4 @@
-
+#-*- coding:utf-8 -*-
 from daemon import Daemon
 import socket
 import select
--- NbNetBase/NetUtils.py
+++ NbNetBase/NetUtils.py
@@ -1,5 +1,4 @@
-
-
+#-*- coding:utf-8 -*-
 from inspect import currentframe
 import socket
 import select
